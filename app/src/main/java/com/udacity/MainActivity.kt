dadec3a18b44761a6e0c89f835446fb082b4cb79
AnaStanescu22 <stanescuanamaria22@yahoo.com>
2022-04-06 11:34:59 +0100

    Implemenet content_main.xml

---
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.udacity
+
+import android.app.DownloadManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var downloadID: Long = 0
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var action: NotificationCompat.Action
+
+    private var url: String? = null
+    private var fileName = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        custom_button.setOnClickListener {
+            download()
+        }
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        }
+    }
+
+    private fun download() {
+        val request =
+            DownloadManager.Request(Uri.parse(URL))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            when (view.getId()) {
+                R.id.glide -> {
+                    url = GLIDE_URL
+                    fileName = getString(R.string.glide)
+                }
+
+                R.id.load_app -> {
+                    url = URL
+                    fileName = getString(R.string.load_app)
+                }
+
+                R.id.retrofit -> {
+                    url = RETROFIT_URL
+                    fileName = getString(R.string.retrofit)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit"
+        private const val URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val CHANNEL_ID = "channelId"
+
+        private const val SUCCESS = "Success"
+
+        private const val FAILED = "Failed"
+    }
+
+}
